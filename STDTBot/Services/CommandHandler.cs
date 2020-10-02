a94c03336d6c9b9ea66fe150d4120613d57ae3d2
Fenissa <fenissa@xy.z>
2020-10-02 19:28:48 +0100

    Initial Command Handler + Module Setup

---
+++ STDTBot/Services/CommandHandler.cs
@@ -0,0 +1,55 @@
+﻿using Discord.Commands;
+using Discord.WebSocket;
+using Microsoft.Extensions.Configuration;
+using NLog;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace STDTBot.Services
+{
+    class CommandHandler
+    {
+        private readonly Logger _log = LogManager.GetCurrentClassLogger();
+        private readonly DiscordSocketClient _client;
+        private readonly CommandService _commands;
+        private readonly IConfigurationRoot _config;
+        private readonly IServiceProvider _provider;
+
+        public CommandHandler(DiscordSocketClient discord, CommandService commands, IConfigurationRoot config, IServiceProvider services)
+        {
+            _client = discord;
+            _commands = commands;
+            _config = config;
+            _provider = services;
+
+            _client.MessageReceived += OnMessageReceieved;
+        }
+
+        private async Task OnMessageReceieved(SocketMessage message)
+        {
+            var msg = message as SocketUserMessage;
+            var context = new SocketCommandContext(_client, msg);
+
+            if (msg == null)
+                return;
+
+            if (msg.Author == _client.CurrentUser)
+                return;
+
+            int argPos = 0;
+            if (msg.HasStringPrefix(_config["prefix"], ref argPos) || msg.HasMentionPrefix(_client.CurrentUser, ref argPos))
+            {
+                _log.Info($"{msg.Author.Username} (in {msg?.Channel?.Name}/{context?.Guild?.Name}) is trying to execute: " + msg.Content);
+                var result = await _commands.ExecuteAsync(context, argPos, _provider);
+
+                if (!result.IsSuccess)
+                {
+                    _log.Error(result.ToString());
+                    await msg.DeleteAsync().ConfigureAwait(false);
+                }
+            }
+        }
+    }
+}
